a2448db57e576b5d99da051761bd74631b0b7f24
David Cimini <36805925+typesalot@users.noreply.github.com>
2024-12-14 18:56:43 -0500

    day11 part2

--- 2024/README.md
+++ 2024/README.md
@@ -12,7 +12,7 @@
 | 8   | :green_circle: | :green_circle:  |
 | 9   | :green_circle: | :green_circle:  |
 | 10  | :green_circle: | :green_circle:  |
-| 11  | :black_circle: | :black_circle:  |
+| 11  | :green_circle: | :green_circle:  |
 | 12  | :black_circle: | :black_circle:  |
 | 13  | :black_circle: | :black_circle:  |
 | 14  | :black_circle: | :black_circle:  |
@@ -35,3 +35,5 @@
 Path finding both to unique destinations and unique paths. I misunderstood the problem initially and solved Part2 as Part1. Since the max depth is only 9, I used recursion to navigate the map. I also took a dynamic programming approach to keep a history of traveled score/rating per location to avoid re-navigating previous paths.
 
 ### Day 11: Plutonian Pebbles
+
+A lot of common subcases that can be cached to avoid re-calculation, with the added complexity that the cache needs to be blink-level aware. I used recursion as well since Part2's 75 blinks means a manageable function depth.
--- 2024/day11.cpp
+++ 2024/day11.cpp
@@ -2,7 +2,8 @@
 
 class Day11 : public testing::Test {
   protected:
-    vector<int> stones;
+    vector<uint64_t>                             stones;
+    array<unordered_map<uint64_t, uint64_t>, 75> cache;
 
     void SetUp() override {
       string fname = getInputFile(2024, 11);
@@ -14,9 +15,9 @@ class Day11 : public testing::Test {
         stones.push_back(stoi(p));
     }
 
-    uint32_t digit_count(uint32_t num) {
-      uint32_t p = 0;
-      uint32_t n = num;
+    uint64_t digit_count(uint64_t num) {
+      uint64_t p = 0;
+      uint64_t n = num;
       while (n) {
         p++;
         n /= 10;
@@ -25,10 +26,10 @@ class Day11 : public testing::Test {
       return p;
     }
 
-    auto split_num(uint32_t num) -> pair<uint32_t, uint32_t> {
-      uint32_t cnt = digit_count(num) / 2;
-      uint32_t p   = 1;
-      uint32_t n   = num;
+    auto split_num(uint64_t num) -> pair<uint64_t, uint64_t> {
+      uint64_t cnt = digit_count(num) / 2;
+      uint64_t p   = 1;
+      uint64_t n   = num;
       while (cnt--) {
         p *= 10;
         n /= 10;
@@ -36,37 +37,38 @@ class Day11 : public testing::Test {
       return {n, num % p};
     }
 
-    void blink() {
-      auto itr = stones.begin();
+    uint64_t blink(uint64_t num, uint32_t cnt) {
+      uint64_t total = 1;
 
-      while (itr != stones.end()) {
-        if (*itr == 0) {
-          *itr = 1;
-          itr++;
-        } else if (digit_count(*itr) % 2 == 0) {
-          auto n      = itr + 2;
-          auto [u, l] = split_num(*itr);
+      if (cnt != 0) {
+        if (cache[cnt - 1].contains(num))
+          return cache[cnt - 1][num];
 
-          *itr = l;
-          itr  = stones.insert(itr, u);
-
-          itr++;
+        if (num == 0) {
+          total = blink(1, cnt - 1);
+        } else if (digit_count(num) % 2 == 0) {
+          auto [u, l] = split_num(num);
+          total       = blink(u, cnt - 1) + blink(l, cnt - 1);
         } else {
-          *itr *= 2024;
-          itr++;
+          total = blink(num * 2024, cnt - 1);
         }
+
+        cache[cnt - 1][num] = total;
       }
+
+      return total;
     }
 
-    uint64_t numStones(uint32_t blinkCnt) {
-      while (blinkCnt--)
-        blink();
-      return stones.size();
+    uint64_t numStones(uint64_t cnt) {
+      uint64_t total = 0;
+      for (auto s : stones)
+        total += blink(s, cnt);
+      return total;
     }
 };
 
 TEST_F(Day11, SplitNum) {
-  uint32_t u, l;
+  uint64_t u, l;
 
   tie(u, l) = split_num(12);
   EXPECT_EQ(u, 1);
@@ -89,4 +91,14 @@ TEST_F(Day11, Part1Example) {
   stones = {125, 17};
   answer = numStones(25);
   EXPECT_EQ(answer, 55312);
+}
+
+TEST_F(Day11, Part1) {
+  uint64_t answer = numStones(25);
+  EXPECT_EQ(answer, 217812);
+}
+
+TEST_F(Day11, Part2) {
+  uint64_t answer = numStones(75);
+  EXPECT_EQ(answer, 259112729857522);
 }
