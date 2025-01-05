034e44322cd52e7affd50be303a34e1b41bf8233
David Cimini <36805925+typesalot@users.noreply.github.com>
2025-01-04 21:26:03 -0800

    d21p1 hacky solution

--- 2024/day21.cpp
+++ 2024/day21.cpp
@@ -7,49 +7,61 @@ using namespace std;
 
 class Day21 : public aoc_2024 {
   protected:
-    using point = point<int8_t>;
+    using point = point<int16_t>;
 
-    vector<string>               sequences;
+    vector<string> sequences;
+
     static const array<point, 4> directions;
+    static constexpr int         north = 0;
+    static constexpr int         east  = 1;
+    static constexpr int         south = 2;
+    static constexpr int         west  = 3;
 
     struct keypad {
-        unordered_map<char, uint8_t> keys;  // key positions, flattened
-        point                        src;   // current x,y position
-        point                        pivot;
+        unordered_map<char, uint16_t> keys;  // key positions, flattened
+        point                         src;   // current x,y position
+        point                         dst;
+        vector<string>                choices;
+        point                         avoid;
 
         keypad(const string& labels) : keys(labels.size()) {
           for (auto l : labels)
             keys[l] = labels.find(l);
           src   = point::expand(keys['A'], 3);
-          pivot = point::expand(keys['*'], 3) + point(1, 0);
+          avoid = point::expand(keys['*'], 3);
         }
 
-        string move(char c) {
-          string presses;
-
-          point dst = point::expand(keys[c], 3);
+        vector<string> move(char c) {
+          dst       = point::expand(keys[c], 3);
           point mov = dst - src;
+          point none;
 
-          if (src.y == pivot.y) {
-            move_y(presses, mov);
-            move_x(presses, mov);
-          } else {
-            move_x(presses, mov);
-            move_y(presses, mov);
-          }
-          presses += 'A';
+          choices.clear();
 
+          gen(src, none, mov.x, mov.y, "");
           src = dst;
 
-          return presses;
+          return choices;
         }
 
-        void move_x(string& presses, const point& d) {
-          presses.append(abs(d.x), (d.x < 0) ? '<' : '>');
-        }
+      private:
+        void gen(const point& p, const point& dir, int x, int y, string s) {
+          point next = p + dir;
+          if (next == avoid)
+            return;
+          if (next == dst) {
+            choices.emplace_back(s + 'A');
+            return;
+          }
 
-        void move_y(string& presses, const point& d) {
-          presses.append(abs(d.y), (d.y < 0) ? '^' : 'v');
+          if (x < 0)
+            gen(next, directions[west], x + 1, y, s + '<');
+          else if (x > 0)
+            gen(next, directions[east], x - 1, y, s + '>');
+          if (y < 0)
+            gen(next, directions[north], x, y + 1, s + '^');
+          else if (y > 0)
+            gen(next, directions[south], x, y - 1, s + 'v');
         }
     };
 
@@ -77,17 +89,61 @@ class Day21 : public aoc_2024 {
       uint32_t size = 0;
 
       string presses;
-      string tmp1;
-      string tmp2;
-      for (auto c : seq)
-        tmp1 += numeric.move(c);
-      for (auto t1 : tmp1)
-        tmp2 += mov1.move(t1);
-      for (auto t2 : tmp2)
-        presses += mov2.move(t2);
+
+      int low = numeric_limits<int>::max();
+
+      vector<string> all0;
+      vector<string> all1;
+      vector<string> all2;
+      for (auto s : seq) {
+        auto choices = numeric.move(s);
+        all0         = combine(all0, choices);
+      }
+
+      for (const auto& c : all0) {
+        vector<string> a;
+
+        for (auto s : c) {
+          auto choices = mov1.move(s);
+          a            = combine(a, choices);
+        }
+
+        int a_low = numeric_limits<int>::max();
+        for (auto n : a)
+          a_low = std::min<int>(a_low, n.length());
+        if (a_low <= low) {
+          if (a_low == low)
+            all1.insert(all1.end(), a.begin(), a.end());
+          else
+            all1 = a;
+          low = a_low;
+        }
+      }
+
+      low = numeric_limits<int>::max();
+
+      for (const auto& c : all1) {
+        vector<string> a;
+
+        for (auto s : c) {
+          auto choices = mov2.move(s);
+          a            = combine(a, choices);
+        }
+
+        int a_low = numeric_limits<int>::max();
+        for (auto n : a)
+          a_low = std::min<int>(a_low, n.length());
+        if (a_low < low) {
+          all2 = a;
+          low  = a_low;
+        }
+      }
+
+      // for (auto& a : all2)
+      //   cout << a << endl;
 
       uint32_t n = atoi(seq.substr(0, seq.length() - 1).c_str());
-      uint32_t s = presses.size();
+      uint32_t s = low;
 
       return n * s;
     }
@@ -98,31 +154,58 @@ class Day21 : public aoc_2024 {
         total += getComplexity(seq);
       return total;
     }
+
+    auto combine(const vector<string>& l, const vector<string>& r) -> vector<string> {
+      if (l.size() == 0)
+        return r;
+      if (r.size() == 0)
+        return l;
+
+      vector<string> result;
+      for (const auto& s : l)
+        for (const auto& t : r)
+          result.emplace_back(s + t);
+      return result;
+    };
 };
 
-decltype(Day21::directions) Day21::directions = {
-    point{0, -1},  // north
-    point{1, 0},   // east
-    point{0, 1},   // south
-    point{-1, 0}   // west
-};
+decltype(Day21::directions) Day21::directions = {point{0, -1},   // north
+                                                 point{1, 0},    // east
+                                                 point{0, 1},    // south
+                                                 point{-1, 0}};  // west
 
 TEST_F(Day21, NumericAvoid) {
+  vector<string> c;
+  vector<string> n;
+
   // Avoids *
-  EXPECT_EQ(numeric.move('0'), "<A");
-  EXPECT_EQ(numeric.move('1'), "^<A");
-  EXPECT_EQ(numeric.move('0'), ">vA");
+  n = numeric.move('0');
+  c = combine(c, n);
+  n = numeric.move('1');
+  c = combine(c, n);
+  n = numeric.move('0');
+  c = combine(c, n);
+
+  EXPECT_EQ(c.size(), 1);
+  EXPECT_EQ(c[0], "<A^<A>vA");
 }
 
 TEST_F(Day21, Numeric029A) {
-  string s;
-  s += numeric.move('0');
-  s += numeric.move('2');
-  s += numeric.move('9');
-  s += numeric.move('A');
+  vector<string> c;
+  vector<string> n;
+
+  n = numeric.move('0');
+  c = combine(c, n);
+  n = numeric.move('2');
+  c = combine(c, n);
+  n = numeric.move('9');
+  c = combine(c, n);
+  n = numeric.move('A');
+  c = combine(c, n);
 
   array<string, 3> p = {"<A^A>^^AvvvA", "<A^A^>^AvvvA", "<A^A^^>AvvvA"};
-  EXPECT_NE(find(p.begin(), p.end(), s), p.end());
+  for (const auto& r : c)
+    EXPECT_NE(find(p.begin(), p.end(), r), p.end());
 }
 
 TEST_F(Day21, Part1Example) {
@@ -139,5 +222,5 @@ TEST_F(Day21, Part1Example) {
 
 TEST_F(Day21, Part1) {
   auto answer = getTotalComplexity();
-  EXPECT_EQ(answer, 293472);  // too high
+  EXPECT_EQ(answer, 278568);
 }
