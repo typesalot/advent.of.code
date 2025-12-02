a5135394e9cf9f264dac3b8280b87d70135606ef
David Cimini <36805925+typesalot@users.noreply.github.com>
2025-12-01 17:02:54 -0800

    2025, day1, part2

--- 2025/day1.py
+++ 2025/day1.py
@@ -1,12 +1,29 @@
 import os
 import time
 
-def parseInput():
+sample = """
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"""
+
+def parseInput(useSample=False):
   input = []
-  assets_path = os.path.expanduser("~/assets/advent.of.code/2025")
-  f = open(os.path.join(assets_path,'day1.txt'),'r')
-  lines = f.readlines()
-  f.close()
+  lines = []
+  if not useSample:
+    assets_path = os.path.expanduser("~/assets/advent.of.code/2025")
+    f = open(os.path.join(assets_path,'day1.txt'),'r')
+    lines = f.readlines()
+    f.close()
+  else:
+    lines = [ l for l in sample.splitlines() if l != "" ]
   for l in lines:
     direction = l[0]
     amount = int(l[1:].strip())
@@ -29,12 +46,19 @@ def part2(input):
   cur = 50
   cnt = 0
   for i in input:
-    if i[0] == 'L':
-      cur = ( cur - i[1] + 100 ) % 100
+    direction = i[0]
+    ticks = i[1]
+    if direction == 'L':
+      if cur <= ticks:
+        cnt = cnt + int(( ticks - cur ) / 100)
+        if cur != 0:
+          cnt = cnt + 1
+      cur = ( cur - ticks + 100 ) % 100
     else:
-      cur = ( cur + i[1] - 100 ) % 100
-    if cur == 0:
-      cnt = cnt + 1
+      t = cur + ticks
+      if t >= 100:
+        cnt = cnt + int( t  / 100 )
+      cur = ( t - 100 ) % 100
   return cnt
 
 print("2025, Day1")
@@ -46,6 +70,7 @@ end = time.perf_counter()
 elapsed_ms = (end - start) * 1000
 print(f"Part1: {result}, Elapsed: {elapsed_ms:.3f} ms")
 
+input = parseInput()
 start = time.perf_counter()
 result = part2(input)
 end = time.perf_counter()
