8a5119d2db967aed0dc5fcb19b392fb529d385d2
David <36805925+typesalot@users.noreply.github.com>
2025-12-03 15:59:34 -0800

    2025, day3, script cleanup

---
+++ 2025/day3.py
@@ -0,0 +1,22 @@
+import os
+from util import *
+
+def parse_input(lines):
+  input = []
+
+  return input
+
+def part1(input):
+  return 0
+
+def part2(input):
+  return 0
+
+input_sample = read_sample(parse_input)
+input = read_input(parse_input)
+
+time_function("Part1 Sample",part1,input_sample)
+time_function("Part1",part1,input)
+
+time_function("Part2 Sample",part2,input_sample)
+time_function("Part2",part2,input)
--- 2025/util.py
+++ 2025/util.py
@@ -1,4 +1,6 @@
 import time
+import os
+import inspect
 
 def time_function(label,func,input):
   print(f"Running {label}... ", end = "")
@@ -7,3 +9,31 @@ def time_function(label,func,input):
   end = time.perf_counter()
   elapsed = (end - start) * 1000
   print(f"done ({elapsed:,.2f}ms). Result = {result}")
+
+def read_sample(parse):
+   return read_input(parse,True)
+
+def read_input(parse, useSample = False):
+  lines = []
+
+  # walk up the stack until we find the parent day##.py filename to use as a key
+  # to grab the corresponding input
+  fname = __file__
+  stack_index = 1
+  while fname == __file__:
+    frame = inspect.stack()[stack_index]
+    fname = frame.filename
+    stack_index = stack_index + 1
+    
+  if useSample:
+    fname = os.path.basename(fname).replace('.py', 'sample.txt')
+  else:
+    fname = os.path.basename(fname).replace('.py', '.txt')
+
+  filePath = os.path.expanduser(os.path.join("~","assets", "advent.of.code", "2025"))
+  fname = os.path.join(filePath,fname)
+
+  f = open(fname,'r')
+  lines = f.readlines()
+  f.close()
+  return parse(lines)
